6d04474ba2d2e9e023f0eae5a79b4b2f492521f4
Trisonweru <isaactrisonwaweru@gmail.com>
2022-03-24 16:45:38 +0300

    fourth commit to blog

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -1,18 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
 import React from 'react';
 
 function Author({ author }: any) {
   return (
     <div className='bg-black bg-opacity-80 mb-8 mt-20 p-12 relative rounded-lg text-center'>
-      <div className='-top-14 absolute left-0 right-2'>
-        <img
+      <div className='-top-14 absolute left-0 right-0'>
+        <Image
           height='100px'
           width='100px'
+          unoptimized
           alt={author.name}
           className='align-middle rounded-full'
           src={author.photo.url}
         />
       </div>
+      <h3 className='font-bold my-4 text-white text-xl'>{author.name}</h3>
+      <p className='text-md text-white'>{author.bio}</p>
     </div>
   );
 }
--- src/components/CommentsForm.tsx
+++ src/components/CommentsForm.tsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 function CommentsForm({ slug }: any) {
-  return <div>CommentsForm</div>;
+  const [error, setError] = useState(false);
+  const [localStorage, showLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commentEL = useRef(null);
+  const nameEL = useRef(null);
+  const emailEL = useRef(null);
+  const storeDataEL = useRef(null);
+
+  const handleCommentSubmission = () => {
+    setError(false);
+    const { value: comment }: any = commentEL.current;
+    const { value: name }: any = nameEL.current;
+    const { value: email }: any = emailEL.current;
+    if (!comment || !name || !email) {
+      setError(true);
+      return;
+    }
+  };
+  return (
+    <div className='bg-white mb-8 p-8 pb-12 rounded-lg shadow-lg'>
+      <h3 className='border-b font-semibold mb-8 pb-4 text-xl'>CommentsForm</h3>
+      <div className='gap-4 grid grid-cols-1 mb-4'>
+        <textarea
+          ref={commentEL}
+          className='bg-gray-100 outline-none p-4 rounded-lg text-gray-700 w-full focus:ring-gray-200'
+          placeholder='Comment'
+          name='comment'
+        />
+      </div>
+      <div className='gap-4 grid grid-cols-1 mb-4 lg:grid-cols-2'>
+        <input
+          ref={nameEL}
+          className='bg-gray-100 outline-none px-4 py-2 rounded-lg text-gray-700 w-full focus:ring-gray-200'
+          placeholder='Name'
+          name='name'
+        />
+        <input
+          ref={emailEL}
+          className='bg-gray-100 outline-none px-4 py-2 rounded-lg text-gray-700 w-full focus:ring-gray-200'
+          placeholder='Email'
+          name='email'
+        />
+      </div>
+      {error && (
+        <p className='text-red-500 text-xs'>All fields are required.</p>
+      )}
+      <div className='mt-8'>
+        <button
+          type='button'
+          onClick={handleCommentSubmission}
+          className='bg-pink-600 cursor-pointer duration-500 ease-in-out inline-block px-8 py-3 rounded-full text-lg text-white transition hover:bg-indigo-900'
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && (
+          <span className='float-right font-semibold mt-3 text-green-500 text-xl'>
+            {' '}
+            Comment submitted for review
+          </span>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default CommentsForm;
